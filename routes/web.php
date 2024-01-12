348e02f5b4f9d3a96d07d950893c42f6342b855a
tombury <tombury59@hotmail.com>
2024-01-12 13:24:17 +0100

    réparation route

--- routes/web.php
+++ routes/web.php
@@ -9,36 +9,36 @@ use App\Http\Controllers\SportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-#----------# Utilisateurs #---------------#
-Route::get('/users', [UserController::class, "getUsers"]);
-Route::get('/user/{userById}', [UserController::class, "getUserById"]);
-Route::post('/user/add', [UserController::class, "addUser"]);
-Route::put('/user/modify/{userId}', [UserController::class, "modifyUser"]);
-Route::delete('/user/delete/{userId}', [UserController::class, 'deleteUser']);
-
-#----------# Sports #---------------#
-Route::get('/sports', [SportController::class, "getSports"]);
-Route::get('/sport/{sportById}', [SportController::class, "getSportById"]);
-
-#----------# Loisirs #---------------#
-Route::get('/loisirs', [LoisirController::class, "getLoisirs"]);
-Route::get('/loisir/{loisirByid}', [LoisirController::class, "getLoisirById"]);
-
-#----------# Professeurs #---------------#
-Route::get('/professeurs', [ProfesseurController::class, "getProfs"]);
-Route::get('/professeur/{profByid}', [ProfesseurController::class, "getProfById"]);
-
-#----------# Services #---------------#
-Route::get('/services', [ServiceController::class, "getServices"]);
-Route::get('/service/{serviceByid}', [ServiceController::class,"getServiceById"]);
-
-#----------# Cours #---------------#
-Route::get('/cours', [CourController::class, "getCours"]);
-Route::get('/cour/{courByid}', [CourController::class,"getCourById"]);
-
-#----------# Annonce #---------------#
-Route::get('/annonces', [AnnonceController::class, "getannonces"]);
-Route::get('/annonce/{annonceByid}', [AnnonceController::class,"getannonceById"]);
-Route::delete('/annonce/delete/{annonceId}', [AnnonceController::class, 'deleteAnnonce']);
-Route::post('/annonce/add', [AnnonceController::class, 'addAnnonce']);
-Route::get('/annonces/last', [AnnonceController::class, "getLastAnnonces"]);
+//#----------# Utilisateurs #---------------#
+//Route::get('/users', [UserController::class, "getUsers"]);
+//Route::get('/user/{userById}', [UserController::class, "getUserById"]);
+//Route::post('/user/add', [UserController::class, "addUser"]);
+//Route::put('/user/modify/{userId}', [UserController::class, "modifyUser"]);
+//Route::delete('/user/delete/{userId}', [UserController::class, 'deleteUser']);
+//
+//#----------# Sports #---------------#
+//Route::get('/sports', [SportController::class, "getSports"]);
+//Route::get('/sport/{sportById}', [SportController::class, "getSportById"]);
+//
+//#----------# Loisirs #---------------#
+//Route::get('/loisirs', [LoisirController::class, "getLoisirs"]);
+//Route::get('/loisir/{loisirByid}', [LoisirController::class, "getLoisirById"]);
+//
+//#----------# Professeurs #---------------#
+//Route::get('/professeurs', [ProfesseurController::class, "getProfs"]);
+//Route::get('/professeur/{profByid}', [ProfesseurController::class, "getProfById"]);
+//
+//#----------# Services #---------------#
+//Route::get('/services', [ServiceController::class, "getServices"]);
+//Route::get('/service/{serviceByid}', [ServiceController::class,"getServiceById"]);
+//
+//#----------# Cours #---------------#
+//Route::get('/cours', [CourController::class, "getCours"]);
+//Route::get('/cour/{courByid}', [CourController::class,"getCourById"]);
+//
+//#----------# Annonce #---------------#
+//Route::get('/annonces', [AnnonceController::class, "getannonces"]);
+//Route::get('/annonce/{annonceByid}', [AnnonceController::class,"getannonceById"]);
+//Route::delete('/annonce/delete/{annonceId}', [AnnonceController::class, 'deleteAnnonce']);
+//Route::post('/annonce/add', [AnnonceController::class, 'addAnnonce']);
+//Route::get('/annonces/last', [AnnonceController::class, "getLastAnnonces"]);
