b35aecdb023ea27e614a9e62673ba76367f38a15
tombury <tombury59@hotmail.com>
2024-01-12 13:36:31 +0100

    re -réparation route

--- routes/api.php
+++ routes/api.php
@@ -26,35 +26,35 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('api/users', [UserController::class, "getUsers"]);
-Route::get('api/user/{userById}', [UserController::class, "getUserById"]);
-Route::post('api/user/add', [UserController::class, "addUser"]);
-Route::put('api/user/modify/{userId}', [UserController::class, "modifyUser"]);
-Route::delete('api/user/delete/{userId}', [UserController::class, 'deleteUser']);
+Route::get('/users', [UserController::class, "getUsers"]);
+Route::get('/user/{userById}', [UserController::class, "getUserById"]);
+Route::post('/user/add', [UserController::class, "addUser"]);
+Route::put('/user/modify/{userId}', [UserController::class, "modifyUser"]);
+Route::delete('/user/delete/{userId}', [UserController::class, 'deleteUser']);
 
 #----------# Sports #---------------#
-Route::get('api/sports', [SportController::class, "getSports"]);
-Route::get('api/sport/{sportById}', [SportController::class, "getSportById"]);
+Route::get('/sports', [SportController::class, "getSports"]);
+Route::get('/sport/{sportById}', [SportController::class, "getSportById"]);
 
 #----------# Loisirs #---------------#
-Route::get('api/loisirs', [LoisirController::class, "getLoisirs"]);
-Route::get('api/loisir/{loisirByid}', [LoisirController::class, "getLoisirById"]);
+Route::get('/loisirs', [LoisirController::class, "getLoisirs"]);
+Route::get('/loisir/{loisirByid}', [LoisirController::class, "getLoisirById"]);
 
 #----------# Professeurs #---------------#
-Route::get('api/professeurs', [ProfesseurController::class, "getProfs"]);
-Route::get('api/professeur/{profByid}', [ProfesseurController::class, "getProfById"]);
+Route::get('/professeurs', [ProfesseurController::class, "getProfs"]);
+Route::get('/professeur/{profByid}', [ProfesseurController::class, "getProfById"]);
 
 #----------# Services #---------------#
-Route::get('api/services', [ServiceController::class, "getServices"]);
-Route::get('api/service/{serviceByid}', [ServiceController::class,"getServiceById"]);
+Route::get('/services', [ServiceController::class, "getServices"]);
+Route::get('/service/{serviceByid}', [ServiceController::class,"getServiceById"]);
 
 #----------# Cours #---------------#
-Route::get('api/cours', [CourController::class, "getCours"]);
-Route::get('api/cour/{courByid}', [CourController::class,"getCourById"]);
+Route::get('/cours', [CourController::class, "getCours"]);
+Route::get('/cour/{courByid}', [CourController::class,"getCourById"]);
 
 #----------# Annonce #---------------#
-Route::get('api/annonces', [AnnonceController::class, "getannonces"]);
-Route::get('api/annonce/{annonceByid}', [AnnonceController::class,"getannonceById"]);
-Route::delete('api/annonce/delete/{annonceId}', [AnnonceController::class, 'deleteAnnonce']);
-Route::post('api/annonce/add', [AnnonceController::class, 'addAnnonce']);
-Route::get('api/annonces/last', [AnnonceController::class, "getLastAnnonces"]);
+Route::get('/annonces', [AnnonceController::class, "getannonces"]);
+Route::get('/annonce/{annonceByid}', [AnnonceController::class,"getannonceById"]);
+Route::delete('/annonce/delete/{annonceId}', [AnnonceController::class, 'deleteAnnonce']);
+Route::post('/annonce/add', [AnnonceController::class, 'addAnnonce']);
+Route::get('/annonces/last', [AnnonceController::class, "getLastAnnonces"]);
