56d9075651cd930872a6f016e2118b720a03d80d
tombury <tombury59@hotmail.com>
2024-02-19 19:29:45 +0100

    modification méthode cours 2

--- app/Http/Controllers/CourController.php
+++ app/Http/Controllers/CourController.php
@@ -11,13 +11,14 @@ class CourController extends Controller
     public function getCours()
     {
         // Requête SQL pour récupérer les cours avec les colonnes spécifiées
-        $cours = DB::table('COURS')
-            ->select('COURS.IDMATIERE as Classe', 'LIBELLEMATIERE', 'LIBELLESPECIALITE', 'UTILISATEUR.NOMUTILISATEUR', 'UTILISATEUR.PRENOMUTILISATEUR', 'EMAIL')
-            ->join('CLASSE', 'COURS.IDSPECIALITE', '=', 'CLASSE.IDSPECIALITE')
-            ->join('ETRE_INCLU', 'ETRE_INCLU.IDSPECIALITE', '=', 'COURS.IDSPECIALITE')
-            ->join('ETUDIANT', 'ETUDIANT.IDUTILISATEUR', '=', 'ETRE_INCLU.IDUTILISATEUR')
-            ->join('UTILISATEUR', 'UTILISATEUR.IDUTILISATEUR', '=', 'ETUDIANT.IDUTILISATEUR')
-            ->get();
+//        $cours = DB::table('COURS')
+//            ->select('COURS.IDMATIERE as Classe', 'LIBELLEMATIERE', 'LIBELLESPECIALITE')
+//            ->join('CLASSE', 'COURS.IDSPECIALITE', '=', 'CLASSE.IDSPECIALITE')
+//            ->join('ETRE_INCLU', 'ETRE_INCLU.IDSPECIALITE', '=', 'COURS.IDSPECIALITE')
+//            ->get();
+        $cours = Cour::all();
+
+
 
         return response()->json($cours, 200, [], JSON_UNESCAPED_UNICODE);
     }
