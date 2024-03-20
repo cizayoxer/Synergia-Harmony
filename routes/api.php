22a96499892032a5a0323ded8682c39ab811e06d
tombury <tombury59@hotmail.com>
2024-03-20 11:23:07 +0100

    Revert "prévention romain"
    This reverts commit fcd2bacef35382430931eeb2cc31a7bf6e5a5258.

--- routes/api.php
+++ routes/api.php
@@ -80,5 +80,3 @@ Route::get('/sondage/{sondageByid}', [SondageController::class,"getSondageById"]
 #----------# Events#---------------#
 Route::get("/events/cinema",[EvenementSportifController::class,'getEvenementsCinema']);
 Route::get("/events/sport",[EvenementSportifController::class,'getEvenementsSport']);
-
-
